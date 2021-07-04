d20990b3d242bbf533c4df09c88256958a80696f
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-07-04 16:40:39 -0400

    Add files via upload

---
+++ Armor/SteelChainmail.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Items.Materials.Bars;
+using static Terraria.ModLoader.ModContent;
+
+namespace Trinitarian.Items.Armor
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class SteelChainmail : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Steel Chainmail");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 20;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = ItemRarityID.Green;
+            item.defense = 5;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<SteelBar>(), 15);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
