5f5bd37b370c0425657fba9c966e8dee1d37fee6
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-07-28 22:54:52 -0400

    Hotfix: 0.2.2.2

---
+++ Subclasses/NecromancerLVL2.cs
@@ -0,0 +1,39 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Subclasses
+{
+    public class NecromancerLVL2 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Necromancer: Level 2");
+            Tooltip.SetDefault("Summoner\n Increases summon damage by 6%, and increased summon slots by 2.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+            item.width = 26;
+            item.height = 24;
+            item.rare = ItemRarityID.Green;
+            item.value = Item.sellPrice(0, 0, 0, 1);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.minionDamage += 0.6f;
+            player.maxMinions += 2;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Bone, 45);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+    }
+}
