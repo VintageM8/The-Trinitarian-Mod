53f15a0af880b49ed471eb14eae601da510a2770
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-08-20 21:33:48 -0400

    Added weapons

---
+++ Items/Weapons/Magus/ElementalPurge.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Items.Materials.Bars;
+using static Terraria.ModLoader.ModContent;
+using Trinitarian.Projectiles.Ammo;
+
+namespace Trinitarian.Items.Weapons.Magus
+{
+	public class ElementalPurge : MagusDamageItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Elemental Purge");
+			Item.staff[item.type] = true;
+		}
+
+		public override void SafeSetDefaults()
+		{
+			item.damage = 19;
+			item.width = 112;
+			item.height = 40;
+			item.useTime = 16;
+			item.useAnimation = 16;
+			item.crit = 0;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			item.knockBack = 6f;
+			item.value = Item.sellPrice(0, 1, 60, 0);
+			item.rare = ItemRarityID.Blue;
+			item.UseSound = SoundID.Item5;
+			item.autoReuse = true;
+			item.shoot = ModContent.ProjectileType<ThePurge>();
+			item.shootSpeed = 20f;
+		}
+	}
+}
