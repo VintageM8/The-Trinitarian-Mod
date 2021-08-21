991e975422af16486d444f7c70d0b19389607755
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-08-20 21:24:21 -0400

    Added Uranium Bar

---
+++ Items/Materials/Bars/UraniumBar.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Trinitarian.Items.Materials.RadiatedSubclass;
+
+namespace Trinitarian.Items.Materials.Bars
+{
+	public class UraniumBar : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Uranium Bar");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 20;
+			item.height = 20;
+			item.maxStack = 99;
+			item.value = 750;
+			item.rare = ItemRarityID.Green;
+			item.value = Item.sellPrice(0, 1, 0, 0);
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.autoReuse = true;
+			item.consumable = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.IronBar, 3);
+			recipe.AddIngredient(ModContent.ItemType<Uranium>(), 5);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
