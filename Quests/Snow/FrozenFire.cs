e433fe820555cba2bf0735c775ff89fbfe958433
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-09-24 19:13:52 -0400

    Add files via upload

---
+++ Quests/Snow/FrozenFire.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Quests.Snow
+{
+	public class FrozenFire : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Frozen Fire");
+			Tooltip.SetDefault("In the Great War of Cthulhu, there was a Frozen Dragon, Etoirir. The Army of the Dryads and Paladins where able to defeat her, but her frozen heart lives on and can bring great power to any magic user.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 20;
+			item.height = 24;
+			item.rare = ItemRarityID.Blue;
+			item.value = Item.sellPrice(0, 0, 50, 0);
+			item.maxStack = 999;
+		}
+	}
+}
---
+++ Quests/Snow/SnowQuestBag.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian;
+using static Microsoft.Xna.Framework.Rectangle;
+using Trinitarian.Quests.Snow;
+
+namespace Trinitarian.Quests.Snow
+{
+	public class SnowQuestBag : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Snow Quest Bag");
+			Tooltip.SetDefault("Contains info on Snow Quests\n{$CommonItemTooltip.RightClickToOpen}");
+		}
+
+		public override void SetDefaults()
+		{
+			item.maxStack = 1;
+			item.consumable = true;
+			item.width = 26;
+			item.height = 34;
+			item.rare = ItemRarityID.Blue;
+			item.expert = false;
+		}
+
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void RightClick(Player player)
+		{
+			player.QuickSpawnItem(ModContent.ItemType<HolyLight>(), 1);
+			player.QuickSpawnItem(ModContent.ItemType<FrozenFire>(), 1);
+		}
+	}
+}
