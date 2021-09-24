f5f291a78e4a3cf19427ed65e8d453c3d4f2605e
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-09-24 19:09:53 -0400

    Updated Summon weapons

---
+++ Items/Weapons/Summoner/NjorsMinion.cs
@@ -0,0 +1,46 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Buffs;
+using Trinitarian.Projectiles.Minions;
+using Microsoft.Xna.Framework;
+using static Terraria.ModLoader.ModContent;
+
+namespace Trinitarian.Items.Weapons.Summoner
+{
+    public class NjorsMinion : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Njor's Minion");
+            ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
+            ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 12;
+            item.knockBack = 3f;
+            item.mana = 15;
+            item.width = 32;
+            item.height = 42;
+            item.useTime = 36;
+            item.useAnimation = 36;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = ItemRarityID.Green;
+            item.UseSound = SoundID.Item44;
+            item.noMelee = true;
+            item.summon = true;
+            item.buffType = BuffType<PlanetBuff>();
+            item.shoot = ProjectileType<NjorMinion>();
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            player.AddBuff(item.buffType, 2);
+            position = Main.MouseWorld;
+            return true;
+        }
+    }
+}
