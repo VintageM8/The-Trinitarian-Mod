dcfb3dee78df391cc5a98c5c54de08d1bae1d1ab
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-09-26 11:12:14 -0400

    Update GlobalNPC

---
+++ TrinitarianGlobalNPC.cs
@@ -0,0 +1,55 @@
+using Terraria;
+using System;
+using System.Collections.Generic;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Items.Materials.Parts;
+using Trinitarian.Items.Accessories;
+using Microsoft.Xna.Framework;
+
+namespace Trinitarian
+{
+    public class TrinitarianGlobalNPC : GlobalNPC
+    {
+
+        public override bool InstancePerEntity
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public bool drowning = false;
+
+        public override void ResetEffects(NPC npc)
+        {
+            drowning = false;
+        }
+        public override void UpdateLifeRegen(NPC npc, ref int damage)
+        {
+            if (drowning)
+            {
+                if (npc.lifeRegen > 0)
+                {
+                    npc.lifeRegen = 0;
+                }
+                npc.lifeRegen -= 8; //change this number to how fast you want the debuff to damage the players. Every 2 is 1 hp lost per second
+            }
+        }
+
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+            if (type == NPCID.ArmsDealer)
+            {
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<GunParts>());
+                nextSlot++;
+            }
+
+            if (type == NPCID.WitchDoctor)
+            {
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Dartboard>());
+                nextSlot++;
+            }
+        }
+    }
+}
