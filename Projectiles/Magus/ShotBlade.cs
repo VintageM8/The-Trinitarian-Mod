c64434b960910d81f251df0d04ff8eb8c5f55b21
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-10-16 10:30:01 -0400

    Shot blade pushed

---
+++ Projectiles/Magus/ShotBlade.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Projectiles.Magus
+{
+    public class ShotBlade : ModProjectile
+    {
+        private int timer = 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blade");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 24;
+            projectile.height = 18;
+            projectile.timeLeft = 100;
+            projectile.penetrate = -1;
+            projectile.hostile = false;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+        }
+        public override void AI()
+        {
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90f);
+            timer++;
+            if (timer > 20)
+            {
+                projectile.tileCollide = true;
+            }
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.Venom, 320);
+        }
+    }
+}
