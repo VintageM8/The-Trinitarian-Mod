6ed7b0787792c88d864474de4795c2bc4849f95e
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-06 14:31:25 -0400

    Updated galactic souls/magus shards

--- Items/Materials/RadiatedSubclass/MagusShards.cs
+++ Items/Materials/RadiatedSubclass/MagusShards.cs
@@ -9,7 +9,7 @@ namespace Trinitarian.Items.Materials.RadiatedSubclass
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Magus Shard");
+            DisplayName.SetDefault("Galactic Souls");
             // ticksperframe, frameCount
             Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 4));
             ItemID.Sets.AnimatesAsSoul[item.type] = true;
@@ -26,6 +26,33 @@ namespace Trinitarian.Items.Materials.RadiatedSubclass
             item.maxStack = 999;
             item.value = 1000;
             item.rare = ItemRarityID.Red;
+        }
+
+         public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.FragmentVortex, 1);
+            recipe.AddTile(TileID.Furnaces);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+
+            ModRecipe recipe2 = new ModRecipe(mod);
+            recipe2.AddTile(TileID.Furnaces);
+            recipe2.AddIngredient(ItemID.FragmentNebula, 1);
+            recipe2.SetResult(this);
+            recipe2.AddRecipe();
+
+            ModRecipe recipe3 = new ModRecipe(mod);
+            recipe3.AddTile(TileID.Furnaces);
+            recipe3.AddIngredient(ItemID.FragmentSolar, 1);
+            recipe3.SetResult(this);
+            recipe3.AddRecipe();
+
+            ModRecipe recipe4 = new ModRecipe(mod);
+            recipe4.AddTile(TileID.Furnaces);
+            recipe4.AddIngredient(ItemID.FragmentStardust, 1);
+            recipe4.SetResult(this);
+            recipe4.AddRecipe();
         }
     }
 }
