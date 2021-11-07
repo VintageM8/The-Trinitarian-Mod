93ebc6e6fb830fd638450942696d98c06df477e6
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-07 16:00:20 -0500

    Quests

---
+++ Items/QuestItems/Paladins/PaladinScroll.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Items.QuestItems.Paladins
+{
+    public class PaladinScroll : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ancient Knowladge");
+            Tooltip.SetDefault("Increases melee damage by 4%\nI would save this");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+            item.width = 26;
+            item.height = 24;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.defense = 5;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.meleeDamage += 0.4f;
+        }
+    }
+}
---
+++ Items/QuestItems/Wizard/WizardToken.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Items.QuestItems.Wizard
+{
+    public class WizardToken : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Wizard Token");
+            Tooltip.SetDefault("Take this to the quest master.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 24;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(0, 0, 50, 0);
+            item.maxStack = 999;
+        }
+    }
+}
