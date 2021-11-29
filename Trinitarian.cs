288fac6a795fdc8fa3e76db581f89654b0b0c4b9
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-29 14:31:27 -0500

    Un fucked the players and stuff

---
+++ Trinitarian.cs
@@ -0,0 +1,59 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System;
+using Trinitarian.NPCs.Bosses.Zolzar;
+using Trinitarian.NPCs.Bosses;
+using Trinitarian.Items.Materials.Parts;
+using Trinitarian.Items.Weapons.Mage;
+using Trinitarian.Items.Weapons.Melee;
+using Trinitarian.Items.Weapons.Ranged;
+using Trinitarian.Items.Weapons.Summoner;
+using static Terraria.ModLoader.ModContent;
+
+namespace Trinitarian
+{
+    public class Trinitarian : Mod
+    {
+	    public static Trinitarian Mod { get; set; }
+	     public static ModHotKey UseAbilty;
+	    public Trinitarian()
+	    {
+		    Mod = this;
+		    
+	    }
+	    public override void Unload()
+	    {
+		    Mod = null;
+		      UseAbilty = null;
+	    }
+	    public override void Load()
+        {
+	       UseAbilty = RegisterHotKey("Use Abilty", "R");
+	    }
+
+		public override void PostSetupContent()
+		{
+			var bossChecklist = ModLoader.GetMod("BossChecklist");
+
+			if (bossChecklist != null)
+			{
+				  bossChecklist.Call("AddBossWithInfo", "Njor, The Frozen Elemental", 2.5f, (Func<bool>)(() => TrinitarianWorld.downedIceBoss), "Use a [i:" + ItemType("FrozenRune") + "] in Snow biome after beating Eye Of Cthulhu");
+				 bossChecklist.Call("AddBossWithInfo", "Zolzar, Beserker Viking", 14.5f, (Func<bool>)(() => TrinitarianWorld.downedViking), "Use a [i:" + ItemType("AsgardsCalling") + "] anywhere, anytime after Moonlord");
+			}
+		}
+
+		public override void AddRecipes()
+        {
+			ModRecipe recipe = new ModRecipe(this);
+			recipe.AddIngredient(ModContent.ItemType<Charcoal>(), 3);
+			recipe.AddIngredient(ItemID.Wood, 8);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(ItemID.Torch, 10); 
+			recipe.AddRecipe();
+		}
+	}
+}
