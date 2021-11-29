a02840e278057497ea576f98e88d12a8a287a7b7
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-29 01:42:57 -0500

    Major push

---
+++ Buffs/Minion/NjorMinionBuff.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Buffs.Minion
+{
+    public class NjorMinionBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Njor's Minion");
+            Description.SetDefault("A fraction of Njor will fight for you");
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.ownedProjectileCounts[mod.ProjectileType("NjorMinion")] > 0)
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+            else
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+        }
+    }
+}
