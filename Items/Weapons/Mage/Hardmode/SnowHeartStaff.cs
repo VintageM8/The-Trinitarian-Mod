1f7a6039ce8e2ac3badc384fabc2d19acd2ab146
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-29 02:10:03 -0500

    Weapons Organized/Major Push

---
+++ Items/Weapons/Mage/Hardmode/SnowHeartStaff.cs
@@ -0,0 +1,58 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Items.Materials.Bars;
+using Trinitarian.Items.Materials.Parts;
+using Trinitarian.Projectiles.Mage;
+using static Terraria.ModLoader.ModContent;
+
+namespace Trinitarian.Items.Weapons.Mage.Hardmode
+{
+    public class SnowHeartStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Heart of the Blizzard");
+            Tooltip.SetDefault("Shoots out snowballs");
+            Item.staff[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 70;
+            item.magic = true;
+            item.mana = 18;
+            item.width = 42;
+            item.height = 40;
+            item.useTime = 33;
+            item.useAnimation = 33;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.noMelee = true;
+            item.knockBack = 6;
+            item.value = Item.sellPrice(0, 23, 0, 0);
+            item.rare = ItemRarityID.Cyan;
+            item.UseSound = SoundID.Item43;
+            item.autoReuse = true;
+            item.shoot = ModContent.ProjectileType<MagicSnowballs>();
+            item.shootSpeed = 0f;
+            item.channel = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<TrueStarSteel>(), 10);
+            recipe.AddIngredient(ItemType<EnchantedIceBall>(), 2);
+            recipe.AddIngredient(ItemID.BlizzardStaff, 1);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-3, 0);
+        }
+    }
+}
