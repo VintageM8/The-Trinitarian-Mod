185e176251ef09fcdc9bd37bf29187502da3333b
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-29 02:21:00 -0500

    Weapons Organization / Major Push

---
+++ Items/Weapons/Summoner/PreHardmode/FleshyWallStaff.cs
@@ -0,0 +1,60 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Buffs.Minion;
+using Trinitarian.Projectiles.Minions;
+using static Terraria.ModLoader.ModContent;
+using Trinitarian.Items.Materials.Bars;
+using Trinitarian.Items.Materials.Parts;
+
+namespace Trinitarian.Items.Weapons.Summoner.PreHardmode
+{
+    public class FleshyWallStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fleshy Wall Staff");
+            Tooltip.SetDefault("Summons a blob of flesh to fight for you");
+            ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
+            ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 19;
+            item.knockBack = 5f;
+            item.mana = 12;
+            item.width = 32;
+            item.height = 42;
+            item.useTime = 36;
+            item.useAnimation = 36;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.value = Item.sellPrice(0, 5, 0, 0);
+            item.rare = ItemRarityID.LightRed;
+            item.UseSound = SoundID.Item44;
+            item.noMelee = true;
+            item.summon = true;
+            item.buffType = BuffType<FleshyWallBuff>();
+            item.shoot = ProjectileType<FleshyWallMinion>();
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            player.AddBuff(item.buffType, 2);
+            position = Main.MouseWorld;
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<TrueStarSteel>(), 8);
+            recipe.AddIngredient(ModContent.ItemType<SummonShards>(), 5);
+            recipe.AddIngredient(ItemID.ObsidianRose, 1);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
