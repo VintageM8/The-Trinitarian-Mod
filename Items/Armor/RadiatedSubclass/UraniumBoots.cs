03c541b6ecdffa3e3b162821de6476c0760cb254
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-29 01:48:32 -0500

    Major Push

---
+++ Items/Armor/RadiatedSubclass/UraniumBoots.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Items.Materials.Bars;
+using static Terraria.ModLoader.ModContent;
+
+namespace Trinitarian.Items.Armor.RadiatedSubclass
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class UraniumBoots : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Uranium Boots");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 16;
+            item.value = Item.sellPrice(0, 0, 50, 0);
+            item.rare = ItemRarityID.Green;
+            item.defense = 5;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<UraniumBar>(), 15);
+            recipe.AddIngredient(ItemID.HellstoneBar, 8);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
