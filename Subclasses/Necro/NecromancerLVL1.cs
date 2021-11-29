006dba41aa9a6b2429cde881defc6c151b22ee08
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-11-29 01:39:26 -0500

    Huge Subclass changes

---
+++ Subclasses/Necro/NecromancerLVL1.cs
@@ -0,0 +1,32 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Subclasses.Necro
+{
+    public class NecromancerLVL1 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Necromancer: Summoner Specialty");
+            Tooltip.SetDefault("Keep this item to obtain new items, weapons, and abilities.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 26;
+            item.height = 24;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(0, 0, 0, 1);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Subclasses/Wizard/WizardLVL1.cs
@@ -0,0 +1,33 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Subclasses.Wizard
+{
+    public class WizardLVL1 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Wizard: Mage Specialty");
+            Tooltip.SetDefault("Keep this item to obtain new items, weapons, and abilities.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+            item.width = 26;
+            item.height = 24;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(0, 0, 0, 1);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+    }
+}
