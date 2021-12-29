5de78a73e3e1b6656be1f47adfd5e944f08daa8d
Hibubs <87348464+Hibubs@users.noreply.github.com>
2021-12-28 16:54:56 -0800

    Races
    Ez
    inherent from the Race class

---
+++ Races/ParentClass.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using Terraria;
+namespace Trinitarian.Races
+{
+    /// <summary>
+    /// This class will serve as a base for every new race added. 
+    /// </summary>
+    public abstract class Race
+    {
+       
+        /// <summary>
+        /// the current(called every tick) passive
+        /// </summary>
+        public virtual void Passive( Player p)
+        {
+            //Main.NewText("no Passive");
+        }
+        public virtual int HpChange() {
+            return 0;
+        }
+
+        public virtual int DefIncrease()
+        {
+            return 0;
+        }
+        public virtual float MoveSpeed()
+        {
+            return 0;
+        }
+        /// <summary>
+        /// If your race has the same changes in stats as another, make this diffrent to stop them from being flagged as the same class in the save/load procces. 
+        /// </summary>
+        /// <returns>amongus</returns>
+        public virtual int ID()
+        {
+            return 0;
+        }
+        
+        /// the int corrsponding to the race. used for save and load + SetFromType
+        /// </summary>
+        /// <returns></returns>
+        public  int GetCurrentRace()
+        {
+            for(int i = 0; i < RacePlayer.Races.Count; i++)
+            {
+                Race r = RacePlayer.Races[i];
+                if (r.DefIncrease() == this.DefIncrease() && this.HpChange() == r.HpChange() && r.MoveSpeed() == this.MoveSpeed() && r.ID()  == this.ID())
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+     //   public int type
+    /// <summary>
+    /// Sets a player based on a number provieded. use get race type for the ID
+    /// also used for saving and load
+    /// MAKE SURE TO UPDATE FOR SAVE AND LOAD WHEN MAKING A NEW CLASS
+    /// </summary>
+    /// <param name="p">player getting their race changed</param>
+    /// <param name="type">Type</param>
+        public static void SetFromType(Player p, int type)
+        {
+            p.GetModPlayer<RacePlayer>().CurrentRace = RacePlayer.Races[type];
+
+
+            
+        }
+        /// <summary>
+        /// Call after changing race
+        /// </summary>
+        /// <param name="p"> the player being effectede</param>
+        public void DoRaceChanges(Player p)
+        {
+            p.moveSpeed += MoveSpeed();
+            p.maxRunSpeed += MoveSpeed();
+            p.statLifeMax2 += HpChange();
+            p.statDefense += DefIncrease();
+        }
+
+
+    }
+}
---
+++ Races/RacePlayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace Trinitarian.Races {
+	public class RacePlayer : ModPlayer
+	{
+        public static List<Race> Races= new List<Race>();
+        public override void Initialize()
+        {
+            var types = typeof(Trinitarian).Assembly.GetTypes();
+
+            foreach (var type in types)
+            {
+                if (type.IsSubclassOf(typeof(Race)) && !type.IsAbstract)
+                {
+                    Race r = Activator.CreateInstance(type) as Race;
+                    Races.Add(r);
+                }
+            }
+
+        }
+        public Race CurrentRace = new Norace();      
+        public override void PreUpdateBuffs()
+        {
+            Main.NewText($"{CurrentRace.GetCurrentRace()}");
+            CurrentRace.DoRaceChanges(player);
+            CurrentRace.Passive(player);
+        }
+        public override TagCompound Save()
+        {
+            return new TagCompound {
+        {"triRaceCur",CurrentRace.GetCurrentRace()},
+      
+    };
+        }
+
+        public override void Load(TagCompound tag)
+        {
+           Race.SetFromType(player,tag.GetInt("triRaceCur"));
+           
+        }
+    }
+   
+}
