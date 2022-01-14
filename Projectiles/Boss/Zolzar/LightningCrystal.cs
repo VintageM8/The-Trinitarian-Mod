e65ed618131c886cb924de871b3c79bb86c4a8d2
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-01-14 03:19:03 -0500

    bossy wossy

---
+++ Projectiles/Boss/Zolzar/LightningCrystal.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Projectiles.Boss.Zolzar
+{
+    public class LightningCrystal : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lightning Crystal");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 106;
+            projectile.height = 124;
+            projectile.penetrate = -1;
+            projectile.friendly = false;
+            projectile.hostile = true;
+            projectile.tileCollide = false;
+            projectile.timeLeft = 540;
+        }
+
+        public override void AI()
+        {
+            if (projectile.ai[1] < 15)
+            {
+                Vector2 origin = projectile.Center;
+                float radius = 15;
+                int numLocations = 30;
+                for (int i = 0; i < 30; i++)
+                {
+                    Vector2 position = origin + Vector2.UnitX.RotatedBy(MathHelper.ToRadians(360f / numLocations * i)) * radius;
+                    Vector2 dustvelocity = new Vector2(0f, 15f).RotatedBy(MathHelper.ToRadians(360f / numLocations * i));
+                    int dust = Dust.NewDust(position, 2, 2, 107, dustvelocity.X, dustvelocity.Y, 0, default, 1);
+                    Main.dust[dust].noGravity = true;
+                }
+                projectile.ai[1]++;
+            }
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+    }
+}
