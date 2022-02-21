fcf0fc456428534371c8b4a9a11d747ad35bf280
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-02-21 11:03:18 -0500

    Boss Depression: Projectile flavor

---
+++ Projectiles/Boss/Zolzar/Meteor.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Projectiles.Boss.Zolzar
+{
+	public class Meteor : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Meteor");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 16;
+			projectile.penetrate = 1;
+			projectile.timeLeft = 6000;
+			projectile.height = 40;
+			projectile.hostile = true;
+			projectile.friendly = false;
+		}
+		int dust1 = 0;
+		public override void AI()
+		{
+			if (projectile.ai[1] > 0f)
+			{
+				projectile.Center = Main.npc[(int)projectile.ai[0]].Center;
+				projectile.ai[1] -= 1f;
+				return;
+			}
+			Vector2 move = new Vector2(0f, 0f);
+			float distance = 400f;
+			Player target = null;
+			for (int k = 0; k < 255; k++)
+			{
+				if (Main.player[k].active && !Main.player[k].dead)
+				{
+					Vector2 newMove = Main.player[k].Center - projectile.Center;
+					float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+					if (distanceTo < distance)
+					{
+						move = newMove;
+						distance = distanceTo;
+						target = Main.player[k];
+					}
+				}
+			}
+			if (target != null)
+			{
+				AdjustMagnitude(ref move);
+				projectile.velocity = (5 * projectile.velocity + move) / 6f;
+				AdjustMagnitude(ref projectile.velocity);
+				if (projectile.Hitbox.Intersects(target.Hitbox))
+				{
+					target.immune = false;
+					target.immuneTime = 0;
+					projectile.Damage();
+					if (!target.immune && target.immuneTime <= 0)
+					{
+						target.immune = true;
+						target.immuneTime = 60;
+					}
+				}
+			}
+			for (int k2 = 0; k2 < 3; k2++)
+			{
+				int dust = Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, DustID.Fire);
+				Main.dust[dust].velocity /= 2f;
+			}
+		}
+
+		private void AdjustMagnitude(ref Vector2 vector)
+		{
+			float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+			if (magnitude > 7f)
+			{
+				vector *= 7f / magnitude;
+			}
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Main.PlaySound(SoundID.Item, (int)projectile.position.X, (int)projectile.position.Y, 14);
+
+			if (Main.rand.Next(0, 4) == 0)
+				Item.NewItem((int)projectile.position.X, (int)projectile.position.Y, projectile.width, projectile.height, 116, 1, false, 0, false, false);
+
+			for (int num625 = 0; num625 < 2; num625++)
+			{
+				float scaleFactor10 = 0.33f;
+				if (num625 == 1)
+				{
+					scaleFactor10 = 0.66f;
+				}
+				if (num625 == 2)
+				{
+					scaleFactor10 = 1f;
+				}
+
+				int num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default, Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13AB6_cp_0 = Main.gore[num626];
+				expr_13AB6_cp_0.velocity.X = expr_13AB6_cp_0.velocity.X + 1f;
+				Gore expr_13AD6_cp_0 = Main.gore[num626];
+				expr_13AD6_cp_0.velocity.Y = expr_13AD6_cp_0.velocity.Y + 1f;
+				num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default, Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13B79_cp_0 = Main.gore[num626];
+				expr_13B79_cp_0.velocity.X = expr_13B79_cp_0.velocity.X - 1f;
+				Gore expr_13B99_cp_0 = Main.gore[num626];
+				expr_13B99_cp_0.velocity.Y = expr_13B99_cp_0.velocity.Y + 1f;
+				num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default, Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13C3C_cp_0 = Main.gore[num626];
+				expr_13C3C_cp_0.velocity.X = expr_13C3C_cp_0.velocity.X + 1f;
+				Gore expr_13C5C_cp_0 = Main.gore[num626];
+				expr_13C5C_cp_0.velocity.Y = expr_13C5C_cp_0.velocity.Y - 1f;
+				num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default, Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13CFF_cp_0 = Main.gore[num626];
+				expr_13CFF_cp_0.velocity.X = expr_13CFF_cp_0.velocity.X - 1f;
+				Gore expr_13D1F_cp_0 = Main.gore[num626];
+				expr_13D1F_cp_0.velocity.Y = expr_13D1F_cp_0.velocity.Y - 1f;
+			}
+
+			projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
+			projectile.width = 10;
+			projectile.height = 10;
+			projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+		}
+	}
+}
+
