28c96c1e94d7a6e6c0c85730111f2a5335f92962
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-04-02 21:39:59 -0400

    Waterstorm Projectiles

---
+++ Content/Projectiles/Subclass/Wizard/WaterstormBubble.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Content.Projectiles.Subclass.Wizard
+{
+    public class WaterstormBubble : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            base.DisplayName.SetDefault("<WaterstormBubble");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 24;
+            projectile.height = 24;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.penetrate = 1;
+            projectile.alpha = 255;
+            projectile.timeLeft = 240;
+            projectile.light = 0.5f;
+            projectile.tileCollide = true;
+        }
+
+        public override void AI()
+        {
+            if (base.projectile.alpha > 0)
+            {
+                base.projectile.alpha -= 25;
+                if (base.projectile.alpha < 0)
+                {
+                    base.projectile.alpha = 0;
+                }
+            }
+            if (projectile.localAI[0] == 0f)
+            {
+                AdjustMagnitude(ref projectile.velocity);
+                projectile.localAI[0] = 1f;
+            }
+            Vector2 move = Vector2.Zero;
+            float distance = 200f;
+            bool target = false;
+            for (int k = 0; k < 200; k++)
+            {
+                if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
+                {
+                    Vector2 newMove = Main.npc[k].Center - projectile.Center;
+                    float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+                    if (distanceTo < distance)
+                    {
+                        move = newMove;
+                        distance = distanceTo;
+                        target = true;
+                    }
+                }
+            }
+            if (target)
+            {
+                AdjustMagnitude(ref move);
+                projectile.velocity = (10 * projectile.velocity + move) / 11f;
+                AdjustMagnitude(ref projectile.velocity);
+            }
+
+            base.projectile.localAI[0] += 1f;
+            if (base.projectile.localAI[0] > 4f)
+            {
+                int num3 = Dust.NewDust(new Vector2(base.projectile.position.X, base.projectile.position.Y), base.projectile.width, base.projectile.height, DustID.Water, 0f, 0f, 100);
+                Main.dust[num3].noGravity = true;
+            }
+        }
+
+        public override void Kill(int TimeLeft)
+        {
+            for (int i = 0; i < 30; i++)
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Water);
+            Main.PlaySound(SoundID.Dig, projectile.position);
+            for (int i = 0; i < Main.rand.Next(1, 2); i++)
+            {
+                Vector2 perturbedSpeed = projectile.velocity.RotatedByRandom(MathHelper.ToRadians(360));
+                Projectile.NewProjectile(projectile.position.X, projectile.position.Y, perturbedSpeed.X, perturbedSpeed.Y, ModContent.ProjectileType<WaternadoBottom>(), 30, 5f, projectile.owner);
+            }
+
+        }
+
+        private void AdjustMagnitude(ref Vector2 vector)
+        {
+            float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            if (magnitude > 15f)
+            {
+                vector *= 15f / magnitude;
+            }
+        }
+    }
+}
