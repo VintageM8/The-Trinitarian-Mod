e5857baeff2d6b06d57ae69e4da9e1f94bf6dc91
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-04-24 20:57:06 -0400

    Update PaladinWraith.cs

--- Content/Subclasses/Paladin/Weapon/PaladinWraith.cs
+++ Content/Subclasses/Paladin/Weapon/PaladinWraith.cs
@@ -6,50 +6,44 @@ using Trinitarian.Content.Projectiles.Subclass.Paladin;
 
 namespace Trinitarian.Content.Subclasses.Paladin.Weapon
 {
-	public class PaladinWraith : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("True Paladin Wrath");
-			Tooltip.SetDefault("'The power of God is with you'");
-		}
+    public class PaladinWraith : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("True Paladin Wrath");
+            Tooltip.SetDefault("After 6 consecutive strikes a holy bomb and a lightning strike will smite all foes.\nExtra damage is dealt to those deemed unholy by the greater powers/\n'The power of God is with you'");
+        }
 
 
-		public override void SetDefaults()
-		{
-			item.damage = 200;
-			item.melee = true;
-			item.width = 70;
-			item.height = 76;
-			item.useTime = 16;
-			item.useAnimation = 16;
-			item.useStyle = ItemUseStyleID.SwingThrow;
-			item.knockBack = 6;
-			item.value = Terraria.Item.sellPrice(1, 0, 0, 0);
-			item.rare = 12;
-			item.UseSound = SoundID.Item1;
-			item.shoot = ModContent.ProjectileType<HolyFire>();
-			item.shootSpeed = 4f;
-			item.autoReuse = true;
-		}
+        public override void SetDefaults()
+        {
+            item.damage = 200;
+            item.melee = true;
+            item.width = 70;
+            item.height = 76;
+            item.useTime = 16;
+            item.useAnimation = 16;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.knockBack = 6;
+            item.value = Terraria.Item.sellPrice(1, 0, 0, 0);
+            item.rare = 12;
+            item.UseSound = SoundID.Item1;
+            //item.shoot = ModContent.ProjectileType<HolyFire>();
+            item.shootSpeed = 4f;
+            item.autoReuse = true;
+        }
 
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			Vector2 origVect = new Vector2(speedX, speedY);
-			//generate the remaining projectiles
-
-			Vector2 newVect = origVect.RotatedBy(System.Math.PI / (Main.rand.Next(72, 1800) / 10));
-			Projectile.NewProjectile(position.X, position.Y, newVect.X, newVect.Y, ModContent.ProjectileType<PalaBeam>(), damage, knockBack, player.whoAmI, 0f, 0f);
-			newVect = origVect.RotatedBy(System.Math.PI / (Main.rand.Next(72, 1800) / 10));
-			Projectile.NewProjectile(position.X, position.Y, newVect.X, newVect.Y, ModContent.ProjectileType<PalaBeam2>(), damage, knockBack, player.whoAmI, 0f, 0f);
-			newVect = origVect.RotatedBy(System.Math.PI / (Main.rand.Next(72, 1800) / 10));
-			Projectile.NewProjectile(position.X, position.Y, newVect.X, newVect.Y, ModContent.ProjectileType<PalaBeam>(), damage, knockBack, player.whoAmI, 0f, 0f);
-			newVect = origVect.RotatedBy(System.Math.PI / (Main.rand.Next(72, 1800) / 10));
-			Projectile.NewProjectile(position.X, position.Y, newVect.X, newVect.Y, ModContent.ProjectileType<PalaBeam2>(), damage, knockBack, player.whoAmI, 0f, 0f);
-			newVect = origVect.RotatedBy(System.Math.PI / (Main.rand.Next(72, 1800) / 10));
-			Projectile.NewProjectile(position.X, position.Y, newVect.X * 1.5f, newVect.Y * 1.5f, ModContent.ProjectileType<PalaBeam>(), damage, knockBack, player.whoAmI, 0f, 0f);
-
-			return false;
-		}
-	}
+        int charger;
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+             charger++;
+            if (charger >= 6)
+            {
+                Main.PlaySound(SoundID.Item, (int)target.position.X, (int)target.position.Y, 14);
+                Terraria.Projectile.NewProjectile(target.Center.X, target.Center.Y, 0f, 0f, ModContent.ProjectileType<HolyBomb>(), damage, knockBack, player.whoAmI);
+                Terraria.Projectile.NewProjectile(target.Center.X, target.Center.Y, 0f, 0f, ModContent.ProjectileType<LightningSpike>(), damage, knockBack, player.whoAmI);
+                charger = 0;
+            }
+        }
+    }
 }
