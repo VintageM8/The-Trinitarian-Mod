67cb01df75d2cc4d955d2912a904462a6b6fdf8d
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-04-24 20:58:37 -0400

    Update LightningSpike.cs

--- Content/Projectiles/Subclass/Paladin/LightningSpike.cs
+++ Content/Projectiles/Subclass/Paladin/LightningSpike.cs
@@ -1,35 +1,88 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
-using Trinitarian.Content.Projectiles.Subclass.Paladin;
+using Trinitarian.Common.Projectiles;
 
 namespace Trinitarian.Content.Projectiles.Subclass.Paladin
 {
 	public class LightningSpike : ModProjectile
 	{
-        public int delay = 0;
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("LightningSpike");
+		}
+
 		public override void SetDefaults()
 		{
-            delay = 0;
-			projectile.width = 6;
-			projectile.height = 6;
-			projectile.aiStyle = 0;
-			projectile.scale = 1f;
-			projectile.penetrate = 1;
-			projectile.timeLeft = 120;
-			projectile.tileCollide = false;
-            projectile.friendly = false;
-            projectile.scale = 2;
+			projectile.width = 80;
+			projectile.height = 80;
+			projectile.timeLeft = 10;
+			projectile.alpha = 255;
+			projectile.maxPenetrate = 1;
+			projectile.hostile = false;
+			projectile.melee = true;
+			projectile.friendly = true;
 		}
-		
+
+		public override void Kill(int timeLeft)
+		{
+			Main.PlaySound(SoundID.Item, (int)projectile.position.X, (int)projectile.position.Y, 14);
+			ProjectileExtras.Explode(projectile.whoAmI, 120, 120,
+				delegate 
+				{
+					for (int i = 0; i < 40; i++)
+					{
+						int num = Dust.NewDust(projectile.position, projectile.width, projectile.height, 226, 0f, -2f, 0, default(Color), .8f);
+						Main.dust[num].noGravity = true;
+						Dust expr_62_cp_0 = Main.dust[num];
+						expr_62_cp_0.position.X = expr_62_cp_0.position.X + ((float)(Main.rand.Next(-50, 51) / 20) - 1.5f);
+						Dust expr_92_cp_0 = Main.dust[num];
+						expr_92_cp_0.position.Y = expr_92_cp_0.position.Y + ((float)(Main.rand.Next(-50, 51) / 20) - 1.5f);
+						if (Main.dust[num].position != projectile.Center)
+						{
+							Main.dust[num].velocity = projectile.DirectionTo(Main.dust[num].position) * 6f;
+						}
+					}
+				});
+			for (int num625 = 0; num625 < 3; num625++)
+			{
+				float scaleFactor10 = 0.33f;
+				if (num625 == 1)
+					scaleFactor10 = 0.66f;
+
+				if (num625 == 2)
+					scaleFactor10 = 1f;
+
+				int num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13AB6_cp_0 = Main.gore[num626];
+				expr_13AB6_cp_0.velocity.X = expr_13AB6_cp_0.velocity.X + 1f;
+				Gore expr_13AD6_cp_0 = Main.gore[num626];
+				expr_13AD6_cp_0.velocity.Y = expr_13AD6_cp_0.velocity.Y + 1f;
+				num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13B79_cp_0 = Main.gore[num626];
+				expr_13B79_cp_0.velocity.X = expr_13B79_cp_0.velocity.X - 1f;
+				Gore expr_13B99_cp_0 = Main.gore[num626];
+				expr_13B99_cp_0.velocity.Y = expr_13B99_cp_0.velocity.Y + 1f;
+				num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13C3C_cp_0 = Main.gore[num626];
+				expr_13C3C_cp_0.velocity.X = expr_13C3C_cp_0.velocity.X + 1f;
+				Gore expr_13C5C_cp_0 = Main.gore[num626];
+				expr_13C5C_cp_0.velocity.Y = expr_13C5C_cp_0.velocity.Y - 1f;
+				num626 = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[num626].velocity *= scaleFactor10;
+				Gore expr_13CFF_cp_0 = Main.gore[num626];
+				expr_13CFF_cp_0.velocity.X = expr_13CFF_cp_0.velocity.X - 1f;
+				Gore expr_13D1F_cp_0 = Main.gore[num626];
+				expr_13D1F_cp_0.velocity.Y = expr_13D1F_cp_0.velocity.Y - 1f;
+			}
+		}
 		public override void AI()
 		{
-            delay--;
-            if(delay <= 0)
-            {
-                delay = 4;
-                if(Main.myPlayer == projectile.owner)
-                    Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0, 48, ModContent.ProjectileType<LightningShard>(), (int)(projectile.damage), 3, Main.myPlayer);
-            }
-        }
+			projectile.Kill();
+		}
 	}
 }
