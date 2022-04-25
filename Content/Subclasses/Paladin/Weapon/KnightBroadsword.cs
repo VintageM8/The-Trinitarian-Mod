2164a5b432c81fbf3d97719a017669749efbdc53
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-04-24 21:01:55 -0400

    Update KnightBroadsword.cs

--- Content/Subclasses/Paladin/Weapon/KnightBroadsword.cs
+++ Content/Subclasses/Paladin/Weapon/KnightBroadsword.cs
@@ -2,33 +2,53 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
-using Trinitarian.Content.Projectiles.Subclass.Paladin;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Utilities;
 using System;
+using Trinitarian.Content.Projectiles.Subclass.Paladin;
 
 namespace Trinitarian.Content.Subclasses.Paladin.Weapon
 {
     public class KnightBroadsword : ModItem
     {
+        public int currentAttack = 1;
+        public override string Texture => "Trinitarian/Content/Subclasses/Paladin/Weapon/KnightBroadsword";
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Knight's Broadsword");
-            Tooltip.SetDefault("Does a slash attack.");
+            ItemID.Sets.ItemNoGravity[item.type] = true;
         }
 
         public override void SetDefaults()
         {
-            item.damage = 23;
+            item.width = item.height = 60;
+            item.damage = 16;
             item.melee = true;
-            item.width = 40;
-            item.height = 40;
-            item.useTime = 10;
-            item.useAnimation = 20;
-            item.useStyle = ItemUseStyleID.SwingThrow;
-            item.knockBack = 2;
-            item.value = Item.buyPrice(gold: 1);
-            item.rare = ItemRarityID.Blue;
-            item.UseSound = SoundID.Item1;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useAnimation = item.useTime = 20;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.shootSpeed = 1f;
+            item.rare = ItemRarityID.Yellow;
             item.autoReuse = true;
+            item.shoot = ModContent.ProjectileType<KnightBroadswordProj>();
+            item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/SwordSwoosh");
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int dir = currentAttack;
+            currentAttack = -currentAttack;
+            Projectile.NewProjectile(position, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI, 0, dir);
+            return false;
+        }
+
+        public override bool UseItem(Player player)
+        {
+            for (int i = 0; i < Math.Min(10, player.GetModPlayer<HolyCombo>().combo / 3); ++i)
+            {
+                Projectile.NewProjectile(player.Center, new Vector2(Main.rand.NextFloat(4, 7) * player.direction, Main.rand.NextFloat(-8, -5)), ModContent.ProjectileType<HolyBomb>(), item.damage, item.knockBack, player.whoAmI);
+            }
+            return true;
         }
     }
 }
