e83313340885b10d78892f6bda065e3f9a2f762e
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-05-08 17:40:37 -0400

    Major Coral bow changes + port

---
+++ Content/Items/Weapons/PreHardmode/Ranged/CoralBow/CoralBow.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Content.Items.Weapons.PreHardmode.Ranged.CoralBow
+{
+    public class CoralBow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Coral Bow");
+            Tooltip.SetDefault("Launches Seashells that speeds up over time\nThe Seashell will also break into smaller shards when hit");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.UseSound = SoundID.Item67;
+            Item.crit = 4;
+            Item.damage = 15;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 60;
+            Item.height = 32;
+            Item.useTime = 65;
+            Item.useAnimation = 65;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 6;
+            Item.value = Item.sellPrice(0, 1, 50, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.autoReuse = true;
+            Item.shoot = Mod.Find<ModProjectile>("CoralBowProj").Type;
+            Item.shootSpeed = 11f;
+        }
+    }
+}
---
+++ Content/Items/Weapons/PreHardmode/Ranged/CoralBow/CrystalCannonShard.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+namespace Trinitarian.Content.Items.Weapons.PreHardmode.Ranged.CoralBow
+{
+    public class CrystalCannonShard : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Crystal Shard");
+            Main.projFrames[Projectile.type] = 3;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 8;
+            Projectile.height = 10;
+            Projectile.aiStyle = 1;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = 5;
+            Projectile.timeLeft = 600;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 1;
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity.Y += 0.2f;
+        }
+        public override void Kill(int timeLeft)
+        {
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+    }
+}
