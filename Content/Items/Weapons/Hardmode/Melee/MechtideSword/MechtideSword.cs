9e355b1e0d09228490c217de21c62764fd9226b0
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-05-11 22:37:26 -0400

    fixes

--- Content/Items/Weapons/Hardmode/Melee/MechtideSword/MechtideSword.cs
+++ Content/Items/Weapons/Hardmode/Melee/MechtideSword/MechtideSword.cs
@@ -1,6 +1,7 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Audio;
 using Terraria.DataStructures;
 using Trinitarian.Content.Items.Materials.Bars;
 using Microsoft.Xna.Framework;
