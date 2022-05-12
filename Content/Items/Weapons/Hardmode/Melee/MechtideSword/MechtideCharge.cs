2057e37ef39fa1c3111ffe017ca6b1c487c120a2
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-05-11 21:16:48 -0400

    Mechtide Sword re-work

---
+++ Content/Items/Weapons/Hardmode/Melee/MechtideSword/MechtideCharge.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Trinitarian.Common.Utilities;
+
+namespace Trinitarian.Content.Items.Weapons.Hardmode.Melee.MechtideSword
+{
+    public class MechtideCharge : ModProjectile
+    {
+        public enum AIState
+        {
+            WithoutAutoAiming,
+            Expectation, // Not used
+            AutoAiming
+        }
+
+        public AIState State { get => (AIState)Projectile.ai[0]; set => Projectile.ai[0] = (float)value; }
+        public float InitSpeed { get => Projectile.ai[1]; set => Projectile.ai[1] = value; }
+        public int Timer { get => (int)Projectile.localAI[0]; set => Projectile.localAI[0] = value; }
+        public int TargetIndex { get; set; } = -1;
+
+        public const float TimerMaxValue = 20;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mechtide Moon");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = -1;
+
+            Projectile.timeLeft = 60 * 15;
+        }
+
+        public override void AI()
+        {
+            Timer = Math.Min(++Timer, (int)TimerMaxValue);
+
+            switch (State)
+            {
+                case AIState.WithoutAutoAiming:
+                    {
+                        Projectile.velocity.Y = Math.Min(Projectile.velocity.Y + 0.3f, 16);
+                    }
+                    break;
+                case AIState.AutoAiming:
+                    {
+                        if (TargetIndex != -1)
+                        {
+                            var npc = Main.npc[TargetIndex];
+                            if (npc == null || !npc.active)
+                            {
+                                TargetIndex = -1;
+                                Projectile.netUpdate = true;
+
+                            }
+                        }
+                        else
+                        {
+                            var target = NPCUtils.NearestNPC(Projectile.Center, 16 * 25, i => i.CanBeChasedBy(Projectile, false) && Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, i.position, i.width, i.height));
+                            var npc = target.npc;
+
+                            if (npc != null)
+                            {
+                                TargetIndex = npc.whoAmI;
+                                Projectile.netUpdate = true;
+                                break;
+                            }
+
+                            Projectile.velocity *= 0.96f;
+                            Projectile.timeLeft--;
+                        }
+                    }
+                    break;
+                default:
+                    Projectile.Kill();
+                    break;
+            }
+
+            Projectile.rotation += Math.Sign(Projectile.velocity.X) * 0.2f;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (Projectile.velocity.X != oldVelocity.X)
+            {
+                Projectile.velocity.X = -oldVelocity.X;
+            }
+
+            if (Projectile.velocity.Y != oldVelocity.Y)
+            {
+                Projectile.velocity.Y = -oldVelocity.Y;
+            }
+
+            Projectile.velocity *= 0.75f;
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.Center);
+
+            OnHit();
+            return false;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (State == AIState.AutoAiming)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            OnHit();
+
+            /*if (target.life <= 0 && target.lifeMax >= 50 && (Main.rand.NextBool(6) ||  NPC.CountNPCS(ModContent.NPCType<WraithSlayer_Samurai>()) < 4)
+            {
+                for (int i = 0; i < 20; i++)
+                    Dust.NewDust(target.position, target.width, target.height, DustID.Wraith);
+
+                Player player = Main.player[Projectile.owner];
+                MethodHelper.SpawnNPC(Projectile.GetSource_FromThis(), (int)target.Center.X, (int)target.Center.Y, ModContent.NPCType<WraithSlayer_Samurai>(), ai3: player.whoAmI);
+            }*/
+        }
+
+        public void OnHit()
+        {
+            if (State == AIState.AutoAiming) return;
+            ChangeState(AIState.AutoAiming);
+        }
+
+        public void ChangeState(AIState state)
+        {
+            if (state == AIState.AutoAiming)
+            {
+                Projectile.timeLeft = 60 * 2;
+            }
+
+            State = state;
+            Projectile.netUpdate = true;
+        }
+
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(TargetIndex);
+        }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            TargetIndex = reader.ReadInt32();
+        }
+    }
+}
---
+++ Content/Items/Weapons/Hardmode/Melee/MechtideSword/MechtideSword.cs
@@ -0,0 +1,81 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Trinitarian.Content.Items.Materials.Bars;
+using Microsoft.Xna.Framework;
+using Trinitarian.Dusts;
+using Microsoft.Xna.Framework.Graphics;
+using Trinitarian.Common.Players;
+
+namespace Trinitarian.Content.Items.Weapons.Hardmode.Melee.MechtideSword
+{
+    public class MechtideSword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mechtide Sword");
+            Tooltip.SetDefault("Every 3 strikes, a moon powered blast will annihilate your foes \nHarnesses the power of the moon.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 70;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 84;
+            Item.height = 90;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 8;
+            Item.value = Item.buyPrice(gold: 1);
+            Item.rare = ItemRarityID.Yellow;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<MechtideCharge>();
+			Item.shootSpeed = 25f;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+           float numberProjectiles = 3 + Main.rand.Next(3);
+			float rotation = MathHelper.ToRadians(20);
+			position += Vector2.Normalize(new Vector2(velocity.X, velocity.Y)) * 45f;
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f;
+				Projectile.NewProjectile(source, position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, 2f, player.whoAmI);
+			}
+			return false;
+		}
+
+        int charger;
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+             charger++;
+            if (charger >= 6)
+            {
+                SoundEngine.PlaySound(SoundID.Item, (int)target.position.X, (int)target.position.Y, 14);
+                Terraria.Projectile.NewProjectile(Item.GetSource_OnHit(target), target.Center.X, target.Center.Y, 0f, 0f, ModContent.ProjectileType<YourMom>(), damage, knockBack, player.whoAmI);
+                charger = 0;
+            }
+        }
+	
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+			if (Main.rand.NextBool(3)) 
+            {
+				//Emit dusts when the sword is swung
+				Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, ModContent.DustType<MechtideDust>());
+			}
+		}
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(1)
+                .AddIngredient(ModContent.ItemType<Mechtide>(), 50)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
