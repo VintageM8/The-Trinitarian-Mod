6219de9ee200e2ab776d947ba0b2f7d00bee05b4
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-05-16 09:59:25 -0400

    Funky coral bow moment

---
+++ Content/Items/Weapons/PreHardmode/Ranged/CoralBow/CoralBowProj.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+namespace Trinitarian.Content.Items.Weapons.PreHardmode.Ranged.CoralBow
+{
+    public class CoralBowProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 40;
+            Projectile.height = 20;
+            Projectile.friendly = true;
+            Projectile.penetrate = 5;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            Projectile.timeLeft = 300;
+            Projectile.aiStyle = 1;
+        }
+        
+        public override bool AI()
+        {
+           Projectile.rotation += 0.8f;
+			if (Projectile.alpha > 55)
+			{
+				Projectile.alpha -= 10;
+				if (Projectile.alpha < 55)
+				{
+					Projectile.alpha = 55;
+				}
+			}
+
+            if (Main.rand.NextBool(6))
+            {
+                Dust.NewDust(Projectile.Center, Projectile.width, Projectile.height, DustID.Water); 
+            }
+
+        }
+       
+        public override void Kill(int TimeLeft)
+		{
+			for (int i = 0; i < 30; i++)
+				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Water);
+			SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+			for (int i = 0; i < Main.rand.Next(1, 2); i++)
+			{
+				Vector2 perturbedSpeed = Projectile.velocity.RotatedByRandom(MathHelper.ToRadians(360));
+				Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.position.X, Projectile.position.Y, perturbedSpeed.X, perturbedSpeed.Y, ModContent.ProjectileType<CorruptHead>(), 40, 5f, Projectile.owner);
+			}
+
+		}
+
+    }
+
+}
