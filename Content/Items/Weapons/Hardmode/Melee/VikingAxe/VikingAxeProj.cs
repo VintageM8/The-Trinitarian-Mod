d7b2693386f7d28db0ee45ca4b831707498de4b6
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-05-22 19:50:15 -0400

    Zolzar Infuser re-work

---
+++ Content/Items/Weapons/Hardmode/Melee/VikingAxe/VikingAxeProj.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using Trinitarian.Common.Players;
+using Trinitarian.Content.Items.Weapons.Hardmode.Ranged.VikingBow;
+using Trinitarian.Content.Projectiles;
+using Terraria.Audio;
+using Terraria.ID;
+
+namespace Trinitarian.Content.Items.Weapons.Hardmode.Melee.VikingAxe
+{
+    public class VikingAxeProj : HeldSword
+    {
+        public override string Texture => "Trinitarian/Content/Items/Weapons/Hardmode/Melee/VikingAxe/ZolzarAxe";
+
+        public override void SetDefaults()
+        {
+            SwingTime = 30;
+            holdOffset = 50f;
+            base.SetDefaults();
+            Projectile.width = Projectile.height = 92;
+            Projectile.friendly = true;
+            Projectile.localNPCHitCooldown = SwingTime;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.DamageType = DamageClass.Melee;
+        }
+
+        public override float Lerp(float val)
+        {
+            return val == 1f ? 1f : (val == 0f
+                ? 0f
+                : (float)Math.Pow(2, val * 10f - 10f) / 2f);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Terraria.Audio.SoundEngine.PlaySound(SoundID.NPCHit42, target.Center);
+
+            for (int i = 0; i < Main.maxPlayers; i++)
+            {
+                float distance = Vector2.Distance(Projectile.Center, Main.player[i].Center);
+                if (distance <= 1050)
+                {
+                    Main.player[i].GetModPlayer<TrinitarianPlayer>().ScreenShake = 1;
+                }
+            }
+
+            Projectile.NewProjectile(Projectile.GetSource_OnHit(target), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<BowAxe>(), Projectile.ai[0] == 0 ? 120 : 20, 2, Projectile.owner);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            // draws the slash
+            Player player = Main.player[Projectile.owner];
+            Texture2D slash = ModContent.Request<Texture2D>("Trinitarian/Assets/Textures/slash_01").Value;
+            float mult = Lerp(Utils.GetLerpValue(0f, SwingTime, Projectile.timeLeft));
+            float alpha = (float)Math.Sin(mult * Math.PI);
+            Vector2 pos = player.Center + Projectile.velocity * (40f - mult * 30f);
+            Main.EntitySpriteDraw(slash, pos - Main.screenPosition, null, Color.Silver * alpha, Projectile.velocity.ToRotation() - MathHelper.PiOver2, slash.Size() / 2, Projectile.scale / 2, SpriteEffects.None, 0);
+            // draws the main blade
+            Texture2D texture = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
+            Vector2 orig = texture.Size() / 2;
+            Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, orig, Projectile.scale, SpriteEffects.None, 0);
+            return false;
+        }
+    }
+}
---
+++ Content/Items/Weapons/Hardmode/Melee/VikingAxe/ZolzarAxe.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Utilities;
+using System;
+
+namespace Trinitarian.Content.Items.Weapons.Hardmode.Melee.VikingAxe
+{
+    public class ZolzarAxe : ModItem
+    {
+        public int currentAttack = 1;
+        public override string Texture => "Trinitarian/Content/Items/Weapons/Hardmode/Melee/VikingAxe/ZolzarAxe";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Zolzar's Infuser");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 92;
+            Item.damage = 16;
+            Item.DamageType = DamageClass.Melee;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.useAnimation = Item.useTime = 20;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shootSpeed = 1f;
+            Item.rare = ItemRarityID.Yellow;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<VikingAxeProj>();
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            int dir = currentAttack;
+            currentAttack = -currentAttack;
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 0, dir);
+            return false;
+        }
+    }
+}
