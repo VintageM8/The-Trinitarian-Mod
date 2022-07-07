a4567a621c0edea1743b55798c4afd70814167a3
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-07-07 16:53:39 -0400

    adding files

---
+++ Common/TrinitarianMenu.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.ID;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Trinitarian.Common
+{
+	public class TrinitarianMenu : ModMenu
+	{
+		public override Asset<Texture2D> Logo => ModContent.Request<Texture2D>("Trinitarian/logo");
+
+		public override int Music => MusicLoader.GetMusicSlot(Mod, "Assets/Sounds/Music/MenuTheme");
+
+
+		public override string DisplayName => "Trinitarian Mod Menu";
+
+		public override void OnSelected()
+		{
+			SoundEngine.PlaySound(SoundID.AbigailSummon); // Plays a thunder sound when this ModMenu is selected
+		}
+	}
+}
