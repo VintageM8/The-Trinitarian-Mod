a299586916e6bfdbc48e5b282690b3d9ae7d09c5
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-07-07 16:47:21 -0400

    final death to magus

---
+++ Content/Items/Accessories/Viking/ZolzarsShield.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinitarian.Content.Items.Accessories.Viking
+{
+    public class ZolzarsShield : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Zolzar's Shield");
+            Tooltip.SetDefault("Increases all class damage by 50%\nReduces damage by 25%");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 28;
+            Item.value = Item.sellPrice(0, 25, 0, 0);
+            Item.rare = ItemRarityID.Expert;
+            Item.accessory = true;
+            Item.defense = 25;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetDamage(DamageClass.Generic) += 0.50f;
+            player.endurance += 0.25f;
+        }
+    }
+}
