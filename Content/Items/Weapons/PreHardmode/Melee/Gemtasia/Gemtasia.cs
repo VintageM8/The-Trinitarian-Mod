17d8ffd6bcaf22ec8ceb52069019279ea9938830
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-07-08 14:07:43 -0400

    Update Gemtasia.cs

--- Content/Items/Weapons/PreHardmode/Melee/Gemtasia/Gemtasia.cs
+++ Content/Items/Weapons/PreHardmode/Melee/Gemtasia/Gemtasia.cs
@@ -15,18 +15,18 @@ public class Gemtasia : ModItem
 
 	public override void SetDefaults()
 	{
-		Item.damage = 16;
-		Item.DamageType = DamageClass.Melee;
-		Item.width = 40;
-		Item.height = 40;
-		Item.useTime = 12;
-		Item.useAnimation = 20;
-		Item.useStyle = 1;
-		Item.knockBack = 3;
-		Item.value = Item.sellPrice(0, 0, 90, 0);
-		Item.rare = ItemRarityID.Orange;
-		Item.UseSound = SoundID.Item1;
-		Item.autoReuse = false;
+	    Item.damage = 29;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 38;
+            Item.useAnimation = 38;
+            Item.useStyle = 1;
+            Item.knockBack = 5;
+            Item.value = Item.sellPrice(0, 0, 90, 0);
+            Item.rare = ItemRarityID.Orange;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = false;
 	}
 
 	public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
@@ -37,11 +37,14 @@ public class Gemtasia : ModItem
 	public override void AddRecipes()
 	{
 		CreateRecipe(1)
-			.AddIngredient(ItemID.Ruby, 2)
-			.AddIngredient(ItemID.StoneBlock, 10)
-			.AddIngredient(ItemID.Bone, 8)
-			.AddIngredient(ItemID.Topaz, 2)
-			.AddTile(TileID.Anvils)
-			.Register();
+		.AddIngredient(ItemID.Ruby, 2)
+                .AddIngredient(ItemID.Sapphire, 3)
+                .AddIngredient(ItemID.Diamond, 2)
+                .AddIngredient(ItemID.Amethyst, 3)
+                .AddIngredient(ItemID.StoneBlock, 10)
+                .AddIngredient(ItemID.Bone, 8)
+                .AddIngredient(ItemID.Topaz, 3)
+		.AddTile(TileID.Anvils)
+		.Register();
 	}
 }
